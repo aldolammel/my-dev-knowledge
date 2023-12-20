6a85e02c2104680eec374dca97e5ad2bda7f4209
Aldo Lammel <aldolammel@gmail.com>
2023-12-20 11:44:46 -0300

    updating api lessons

--- 30-API/creating/2-creating-example.py
+++ 30-API/creating/2-creating-example.py
@@ -49,9 +49,51 @@ def home():
 
 
 @app.route("/books", methods=["GET"])
-def all_books():
-    """ This function converts the book list to json format """
+def products():
+    """ This function converts the book list to json format and print it out """
     return jsonify(books)
+
+
+@app.route("/books/<int:book_id>", methods=["GET"])
+def product(book_id):
+    """ This function shows up a specific book """
+    # For each book inside the books list:
+    for book in books:
+        # If the book ID is the same of the book_id:
+        if book.get("ID") == book_id:
+            # Return the book data in json format:
+            return jsonify(book)
+    # If the book searching fail, return the message:
+    return "Product not found!"
+
+
+@app.route("/books/<int:book_id>/edit", methods=["PUT"])
+def update(book_id):
+    """ This function update an existent product """
+    # To receive an info from the user, use request.get_json():
+    updated_book = request.get_json()
+    for idx, book in enumerate(books):
+        if book.get("ID") == book_id:
+            books[idx].update(updated_book)
+            return jsonify(books[idx])
+    # If the book searching fail, return the message:
+    return "Product not found!"
+
+
+@app.route("/books/add", methods=["POST"])
+def add_product():
+    """ This function adds a new product to the books list """
+    new_product = request.get_json()
+    books.append(new_product)
+    return jsonify(books)
+
+
+@app.route("/books/<int:book_id>/edit", methods=["DELETE"])
+def remove_product(book_id):
+    for idx, book in enumerate(books):
+        if book.get("ID") == book_id:
+            del books[idx]
+            return jsonify(books)
 
 
 app.run(port=5000, host="localhost", debug=True)
