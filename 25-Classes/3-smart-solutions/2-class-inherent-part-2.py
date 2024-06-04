ce09b7a7d3b426a47348be9fc8ab479f4a02d37e
Aldo Lammel <aldolammel@gmail.com>
2024-06-04 19:41:02 -0300

    update

--- 25-Classes/3-smart-solutions/2-class-inherent-part-2.py
+++ 25-Classes/3-smart-solutions/2-class-inherent-part-2.py
@@ -1,52 +1,30 @@
 """
-CLASS INHERENT: SUPER()
-
-    It's a function used to give access to the methods of a parent class.
-    It returns a temporary object of a parent class when used.
+CLASS IN PYTHON: INHERITANCE
+When a class is the parent of other ones.
 
 """
 
 
-class Person:
-    def __init__(self, f_name, l_name):
-        self.f_name = f_name
-        self.l_name = l_name
+# Main class Animal:
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+        self.can_move = True
+
+    def breathe(self):
+        print("Inhale, Exhale.")
 
 
-class Professor(Person):
-    def __init__(self, f_name, l_name, t_status, h_date, f_date):
-        super().__init__(f_name, l_name)
-        self.t_status = t_status
-        self.h_date = h_date
-        self.f_date = f_date
+# Subclass Fish:
+class Fish(Animal):  # Here I'm calling the parent class "Animal".
+    def __init__(self):
+        super().__init__()
+        self.can_swing = True
 
-    def show_profile(self):
-        return (
-            f"Name: {self.f_name} {self.l_name}\n"
-            f"Teaching Status: {self.t_status}\n"
-            f"Hired date: {self.h_date}\n"
-            f"Fired date: {self.f_date}\n"
-        )
+    def breathe(self):
+        super().breathe()
+        print("Doing this underwater.")
 
 
-class Student(Person):
-    def __init__(self, f_name, l_name, l_status, dt_enroll, dt_closure):
-        super().__init__(f_name, l_name)
-        self.l_status = l_status
-        self.dt_enroll = dt_enroll
-        self.dt_closure = dt_closure
-
-    def show_profile(self):
-        return (
-            f"Name: {self.f_name} {self.l_name}\n"
-            f"Learning Status: {self.l_status}\n"
-            f"Enroll date: {self.dt_enroll}\n"
-            f"Closure date: {self.dt_closure}\n"
-        )
-
-
-professor1 = Professor("Pitagoras", "Junior", "Arrested", "2000 a.C.", None)
-print(professor1.show_profile())
-
-student1 = Student("Aldo", "Lammel", "Studying", "01/03/2022", None)
-print(student1.show_profile())
+nemo = Fish()  # Try with Animal() and Fish().
+nemo.breathe()
