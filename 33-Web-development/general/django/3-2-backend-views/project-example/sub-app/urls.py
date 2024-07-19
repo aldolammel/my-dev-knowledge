7d49a758f4ff8ee96bd43467271ce24efbf7767b
Aldo Lammel <aldolammel@gmail.com>
2024-07-19 16:18:44 -0300

    ok

--- 33-Web-development/general/django/3-2-backend-views/project-example/sub-app/urls.py
+++ 33-Web-development/general/django/3-2-backend-views/project-example/sub-app/urls.py
@@ -6,20 +6,32 @@ from . import views
 app_name = 'recipes'
 
 
-# Defining the routes to be used by this sub-app:
-urlpatterns = [
-    # http://127.0.0.1:8000/recipes or http://127.0.0.1:8000/recipes/index.html
-    path('', views.recipes, name='list_view'),
-    # Crucial: this 'name' above is the 'pattern name' you'll use to build the URL's in templates.
-    # http://127.0.0.1:8000/recipes/1
-    path('<int:recipe_id>', views.recipe, name='detail_view'),
-    # Using class-based views:
-    path('testing/', views.RecipesList.as_view())
-]
+# EXAMPLE USING FUNCTION-BASED METHOD:
+
+    # Defining the routes to be used by this sub-app:
+    urlpatterns = [
+        # http://127.0.0.1:8000/recipes or http://127.0.0.1:8000/recipes/index.html
+        path('', views.recipes, name='list_view'),
+        # http://127.0.0.1:8000/recipes/1
+        path('<int:recipe_id>', views.recipe, name='detail_view'),
+        # Crucial: this 'name' above is the 'pattern name' you'll use to build the URL's in templates.
+    ]
+
+# EXAMPLE USING CLASS-BASED METHOD:
+
+    # Defining the routes to be used by this sub-app:
+    urlpatterns = [
+        # http://127.0.0.1:8000/recipes or http://127.0.0.1:8000/recipes/index.html
+        path('classmethod/', views.RecipesList.as_view(), name='list_view'),
+        # http://127.0.0.1:8000/recipes/1
+        path('<int:pk>', views.RecipeDetail.as_view(), name='detail_view'),
+        # Crucial: this 'name' above is the 'pattern name' you'll use to build the URL's in templates.
+    ]
+
 
 
 # IMPORTANT:
-# this 'recipes/' as a folder in the URL, it's defined in the
+# This 'recipes/' as a folder in the URL, it's defined in the
 # config_folder (in the project, this folder has the same name of the project).
 # In config_folder, you find a file called 'urls.py'. There you can
 # define that 'recipes/' (kind of a 'directory' to organize URLs).
--- 33-Web-development/general/django/3-2-backend-views/project-example/sub-app/views.py
+++ 33-Web-development/general/django/3-2-backend-views/project-example/sub-app/views.py
@@ -1,29 +1,48 @@
 from django.shortcuts import render
 from .models import Recipe
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
-def recipes(request):
-    # Collecting all recipes recorded in the Recipe table:
-    recipes = Recipe.objects.all()
-    # If you wanna pass variables to the template, you need this always as dictionary:
-    context = {'recipes': recipes}
-    # Return the data to be rendered with the template when a key of the context dict is called:
-    return render(request, 'recipes/index.html', context)
+
+# EXAMPLE USING CLASS-BASED METHOD:
 
 
-def recipe(request, recipe_id):
-    # Defining the recipe:
-    recipe = Recipe.objects.get(id=recipe_id)
-    # If you wanna pass variables to the template, you need this always as dictionary:
-    context = {'recipe': recipe}
-    # Return the data to be rendered with the template when a key of the context dict is called:
-    return render(request, 'recipes/recipe.html', context)
+    # The same as 'recipes' function but using the class-based method:
+    class RecipesList(ListView):
+        # Attaching the table (defined in some models.py) will feed this view-class:
+        model = Recipe
+        # Defining with template must be used to show the data:
+        template_name = 'recipes/index.html'
+        # Defining the name you will call the data (variable) on the templates:
+            # Custom definition:
+            context_object_name = 'recipes'
+            # Or the default one:
+            # object_list   <-- call this in template to call automatically all data from the model!
+    
+    # The same as 'recipe' function but using the class-based method:
+    class RecipeDetail(DetailView):
+        model = Recipe
+        template_name = 'recipes/recipe.html'
+        context_object_name = 'recipe'
 
 
-class RecipesList(ListView):
-    model = Recipe
-    template_name = 'recipes/index.html'
-    context_object_name = 'recipes'
-    # Check the urls.py file to see how this class-based method is different from the
-    # function-based method.
+
+# EXAMPLE USING FUNCTION-BASED METHOD:
+
+    # The same as the 'RecipesList' class but using the function-based method:
+    def recipes(request):
+        # Collecting all recipes recorded in the Recipe table:
+        recipes = Recipe.objects.all()
+        # If you wanna pass variables to the template, you need this always as dictionary:
+        context = {'recipes': recipes}
+        # Return the data to be rendered with the template when a key of the context dict is called:
+        return render(request, 'recipes/index.html', context)
+
+    # The same as the 'RecipeDetail' class but using the function-based method:
+    def recipe(request, recipe_id):
+        # Collecting a specific recipe in the Recipe table:
+        recipe = Recipe.objects.get(id=recipe_id)
+        context = {'recipe': recipe}
+        return render(request, 'recipes/recipe.html', context)
+
+
