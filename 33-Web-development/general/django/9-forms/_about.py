3cbedab0712c1eeca9568fc59afc999d4c391b6d
Aldo Lammel <aldolammel@gmail.com>
2024-07-21 20:03:11 -0300

    updating

---
+++ 33-Web-development/general/django/9-forms/_about.py
@@ -0,0 +1,115 @@
+
+
+# DJANGO FORMS
+
+    # >> All data that can be input in the database from front-end;
+
+    # >> Let's imagine you got in the models.py file a class to record movies with a few fields to
+    #   fill. Now, to the user add movies from the front-end, we need to create a form class to
+    #   connect the database with the template:
+
+        # 1) In some sub-app folder, create a file called 'forms.py';
+
+        
+        # 2) In the new sub-app forms.py file, create the class:
+
+            # E.g.
+
+                # Importing:
+                from django import forms
+                from .models import Movie
+                
+                # Create a class specifically for this form:
+                class MovieForm(forms.ModelForm):
+
+                    # Call this the built-in class:
+                    class Meta:
+
+                        # Connecting the form with the model/table it will populate:
+                        model = Movie  # assuming it's the table you got in your models.py;
+
+                        # Bring all class attributes/database columns that will be worked by form:
+                        fields = [
+                            'title',
+                            'release_year',
+                            'rating',
+                        ]
+                        # Customizing each field name:
+                        labels = {
+                            'title': 'Movie title',
+                            'release_year': "The year of the movie's release",
+                            'rating': 'Your personnal rating',
+                        }
+                    
+
+    
+        # 3) In the sub-app views.py file, create the function-view or the class-view responsable for
+        #   this form to be used in a template:
+
+            # E.g.
+                
+                # Importing:
+                from django.shortcuts import render, redirect, HttpResponse
+                
+                # Using a function-based method:
+                def movie_add(request):
+                    # When the user submits the form to the database:
+                    if request.method == 'POST':
+                        form = MovieForm(request.POST)
+                        if form.is_valid():
+                            # Finally record the new things on the database:
+                            form.save()
+                            return redirect('movies_namespace:list_view')
+                        else:
+                            return HttpResponse("The form wasn't valid! Try again!")
+                        
+                    # When the user will add a movie:
+                    else:
+                        form = MovieForm()
+                        context = {'form': form}
+                        return render(request, 'movies/add.html', context)
+
+
+
+        # 4) In the sub-app urls.py file, do it:
+
+            # E.g.
+            
+                # Importing:
+                from . import views
+
+                # Namespace:
+                app_name = 'movies_namespace'
+                
+                urlpatterns = [
+                    # Assuming the movie class has a list view (with class-based view):
+                    # http://127.0.0.1:8000/movies/  or  # http://127.0.0.1:8000/movies/index.html
+                    path('', views.MoviesList.as_view(), name='list_view'),
+                    # http://127.0.0.1:8000/movies/new
+                    path('new/', views.movie_add, name='form_new'),
+                ]
+
+
+
+        # 5) In the sub-app template 'movies/add.html', let's apply the form:
+
+            {% block content %}
+                
+                <h1>Adding New Movie</h1>
+
+                <form method="post">
+
+                    <!-- Add this on each form for Django security reason -->
+                    {% csrf_token %}
+
+                    {{ form.as_p }}
+                    OR 
+                    {{ form.as_div }}
+
+                    <button type="submit">Save</button>
+                    
+                </form>
+                
+            {% endblock content %}
+
+
