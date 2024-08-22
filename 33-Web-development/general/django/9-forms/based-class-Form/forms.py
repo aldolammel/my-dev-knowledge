b3d301f58b92f73e1e35b418d6916aa2e9cccbec
Aldo Lammel <aldolammel@gmail.com>
2024-08-22 14:34:39 -0300

    okay

---
+++ 33-Web-development/general/django/9-forms/based-class-Form/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+
+
+# USING FORMS.FORM CLASS
+# Reminder: forms.Form never inputs data in the database because it's not linked with a model class.
+
+
+class ReviewForm(forms.Form):  # That 'Form' in the classname is a convention.
+    user_name = forms.CharField(
+        label='Your name',
+        max_length=40,
+        error_messages={
+            'required': 'Name must not be empty.',
+            'max_length': 'Enter with a shorter name.',
+        },
+    )
+    review_text = forms.CharField(
+        label='Your feedback',
+        max_length=300,
+        widget=forms.Textarea,
+    )
+    rating = forms.IntegerField(
+        label='Your rating',
+        min_value=1,
+        max_value=5,
+    )
+
+
+# ON THE TEMPLATE / FRONT-END:
+# /33-Web-development/general/django/9-forms/based-class-Form/template.html
