bfda3b4d951cd67fa38614c316adc647e5f474dc
Aldo Lammel <aldolammel@gmail.com>
2024-10-31 11:46:56 -0300

    okay

--- 33-Web-development/backend/django/9-forms/customizing-basic-attrs-extra-field.py
+++ 33-Web-development/backend/django/9-forms/customizing-basic-attrs-extra-field.py
@@ -4,20 +4,16 @@
 FORM: BASIC CUSTOMIZING ATTRIBUTES OF AN EXTRA FIELD (NOT FROM CONNECTED MODEL)
 
 
-    >> xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx:
-
+    >> All fields of the form-class that are not from the connected model, I call them 'Extra' fields;
+    
+    >> All extra fields need to be initiated through the form __init__ method to have the chance
+        to be populated by database data if applicable.
 
 
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 
-    A) xxxxxxxxxxxxxxxxxxxxx;
-        
-        Purpose: 
-            xxxxxxxxxxxxxxxxxxxxxxx.
-        
-        Usage: 
-            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.
+    A) Declaring the extra fields similar what you do in models;
 
         E.g.
         
@@ -31,43 +27,42 @@ class EventAttackModelForm(forms.ModelForm):
         # Ordering fields on the form:
         fields = [
             ...,
+            
         ]
         # Simple and static tweaks only for connected model's fields:
         widgets = {
             ...,
         }
         
-    # Extra fields:
-    <HEREEEEEEEEEEEEEEEEEEEEEE>
-    <HEREEEEEEEEEEEEEEEEEEEEEE>
-    <HEREEEEEEEEEEEEEEEEEEEEEE>
-    <HEREEEEEEEEEEEEEEEEEEEEEE>
-    <HEREEEEEEEEEEEEEEEEEEEEEE>
-    <HEREEEEEEEEEEEEEEEEEEEEEE>
+    # Extra (non-model) fields:
+    email = forms.EmailField(
+        required=True,
+        label='E-mail'
+        widget=forms.TextInput(attrs={'class': 'input is-large'}),
+        help_text="You'll need to call this here too.",
+        error_messages={
+            'blank': 'E-mail is required!',
+            'invalid': 'Please, enter a valid e-mail.',
+        },
+    )
+    is_notified_by_email = forms.BooleanField(
+        required=False,
+        label='Notify me by email',
+        widget=forms.CheckboxInput(attrs={'class': 'is-large', 'id': 'is_notified_by_email'}),
+        help_text="You'll need to call this here too.",
+    )
+    language = forms.ModelChoiceField(
+        queryset=Language.objects.filter(status='on'),
+        required=False,
+        label='Language',
+        help_text="You'll need to call this here too.",
+    )
+    last_pwd_update = forms.DateTimeField(
+        required=False,
+        label="Password's last update"
+        widget=forms.TextInput(attrs={'class': 'input is-small'}),
+    )
 
-
-'''         
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-
-
-    B) XXXXXXXXXXXXXXXX;
-
-        Purpose: 
-            XXXXXXXXXXXXXXXXXXXXX
-
-        Usage: 
-            XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-
-
-            
-'''
-
-class EventAttackModelForm(forms.ModelForm):
-    
-    class Meta:
-        ...
         
     def __init__(self, *args, **kwargs):
         '''Built-in method that's called when the form is initializated.'''
@@ -78,12 +73,10 @@ class EventAttackModelForm(forms.ModelForm):
         
         # Extra fields, pre-populating:
         # Unlike fields from connected model, extra fields must be manually linked!
-        <HEREEEEEEEEEEEEEEEEEEEEEE>
-        <HEREEEEEEEEEEEEEEEEEEEEEE>
-        <HEREEEEEEEEEEEEEEEEEEEEEE>
-        <HEREEEEEEEEEEEEEEEEEEEEEE>
-        <HEREEEEEEEEEEEEEEEEEEEEEE>
-        <HEREEEEEEEEEEEEEEEEEEEEEE>
+        self.fields['email'].initial = user.email
+        self.fields['is_notified_by_email'].initial = user.is_notified_by_email
+        self.fields['language'].initial = user.language
+        self.fields['last_pwd_update'].initial = user.last_pwd_update
 
 
 
