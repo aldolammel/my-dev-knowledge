5d7d972d3582c95d088102af853b31ae4148e662
Aldo Lammel <aldolammel@gmail.com>
2025-03-02 20:13:47 -0300

    updating

--- 33-Web-development/backend/django/12-error-pages/401/sub-app/views.py
+++ 33-Web-development/backend/django/12-error-pages/401/sub-app/views.py
@@ -10,3 +10,21 @@ def view_example(request, pk=None):
             return render(request, '401.html', status=401)
 
     # code...
+    
+    
+    
+"""
+PATH:
+
+If the file is only 'file_name.html' it means the html file is alocated
+in /project/templates/file_name.html.
+
+If you need to call a file from an sub-app folder:
+
+Right way:
+'sub-app/file_name.html', it doens't need to explicit set 'templates' folder.
+
+Bad way:
+'sub-app/templates/file_name.html'
+
+"""
