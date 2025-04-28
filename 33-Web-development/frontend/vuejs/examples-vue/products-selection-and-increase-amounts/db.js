921f528d547183ad5879087fcd7d99e5951b8d4d
Aldo Lammel <aldolammel@gmail.com>
2025-04-28 14:13:08 -0300

    okay

---
+++ 33-Web-development/frontend/vuejs/examples-vue/products-selection-and-increase-amounts/db.js
@@ -0,0 +1,84 @@
+// Simulating a database or Json from an external service:
+var products = [
+    {
+        "photo": "img/big-mac.png",
+        "name": "Big Mac",
+        "price": 5.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/mc-chicken.png",
+        "name": "Mc Chicken",
+        "price": 4.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/double-cb.png",
+        "name": "Double Cheese Burger",
+        "price": 2.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/fries.png",
+        "name": "Batata frita",
+        "price": 2.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/nuggets.png",
+        "name": "Mc Nuggets",
+        "price": 3.49,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/salad.png",
+        "name": "Salada",
+        "price": 2.79,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/cola.png",
+        "name": "Coca Cola",
+        "price": 1.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/lipton.png",
+        "name": "Ice Tea",
+        "price": 1.99,
+        "active": false,
+        "quantity": 1
+    },
+    {
+        "photo": "img/water.png",
+        "name": "Água",
+        "price": 1.49,
+        "active": false,
+        "quantity": 1
+    }
+];
+ 
+// Constant:
+const SelfServiceMachine = {
+    // Property as function:
+    data() {
+        // Object:
+        // Returning an object (everything inside this will be accessable through the app)!
+        return {
+            // Property:
+            products: window.products,
+                // If you want to call products attributes through the templates,
+                // you can use {{ products[0].price }} to take the first product price, e.g.
+        }
+    }
+};
+
+// I can call Vue. here because the index.html is already using the VUE CDN:
+Vue.createApp(SelfServiceMachine).mount('#app');  // This '#app' is the main tag id in index.html
