29d8f49621fe1a914db1f5271c61c5d09d3d7525
Aldo Lammel <aldolammel@gmail.com>
2025-05-29 20:07:25 -0300

    vuex updating

--- 33-Web-development/frontend/javascript/examples/fetch-api-on-vuejs/vuex_action_using_fetchApi.js
+++ 33-Web-development/frontend/javascript/examples/fetch-api-on-vuejs/vuex_action_using_fetchApi.js
@@ -1,11 +1,19 @@
+
+/*
+    HOW TO CREATE DATA WITH VUEX IN VUE.JS
+    This file is used by example in:
+        /33-Web-development/frontend/vuejs/3-component-libraries/vuex/1-overview-how-to-create-data.txt
+*/
+
 export default createStore({
     //...
     actions: {
         loadProducts({ commit }) {
+            // This function read an API (json) and set the response to a mutation (updateProducts).
             fetch('https://fakestoreapi.com/products')
                 .then(response => response.json())
                 .then(data => {
-                    commit('setProducts', data)  // setProducts = mutation that the action will call!
+                    commit('updateProducts', data)  // 'updateProducts' is the mutation to call, and 'data' is the products to change!
                 })
         }
     },
