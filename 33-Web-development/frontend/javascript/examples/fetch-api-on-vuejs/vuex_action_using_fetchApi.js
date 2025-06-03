202b1c7e5210409f43316740b25713ad61bfadd6
Aldo Lammel <aldolammel@gmail.com>
2025-06-03 10:20:41 -0300

    vue path update

--- 33-Web-development/frontend/javascript/examples/fetch-api-on-vuejs/vuex_action_using_fetchApi.js
+++ 33-Web-development/frontend/javascript/examples/fetch-api-on-vuejs/vuex_action_using_fetchApi.js
@@ -2,7 +2,7 @@
 /*
     HOW TO CREATE DATA WITH VUEX IN VUE.JS
     This file is used by example in:
-        /33-Web-development/frontend/vuejs/3-component-libraries/vuex/1-overview-how-to-create-data.txt
+        /33-Web-development/frontend/vue/3-component-libraries/vuex/1-overview-how-to-create-data.txt
 */
 
 export default createStore({
