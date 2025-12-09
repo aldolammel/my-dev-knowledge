1a18f69fb05b5c680ac85077f18258b182414393
Aldo Lammel <aldolammel@gmail.com>
2025-12-09 17:44:19 -0300

    django updates

--- Python/Web-development/django/4-cms-admin/method-get_readonly_fields.py
+++ Python/Web-development/django/4-cms-admin/method-get_readonly_fields.py
@@ -4,7 +4,7 @@
 
     The get_readonly_fields() method determines which fields should be read-only in forms, typically in the CMS. So its key points are:
 
-    - t returns a list/tuple of field names that should be displayed as read-only.
+    - It returns a list/tuple of field names that should be displayed as read-only.
     - Fields specified will be visible but cannot be edited.
     - Can be dynamic based on conditions (unlike the static readonly_fields attribute).
 
@@ -25,6 +25,9 @@
 
 class MyModelAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
-        if obj:  # Editing an existing object
-            return ['created_date', 'created_by']
-        return []  # Creating a new object
+        # Get the class-level readonly_fields
+        current = list(self.readonly_fields)
+        # Editing an existing object:
+        if obj:
+            return current + ["element_type"]
+        return current
